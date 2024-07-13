1dc6f64f3291e16a42f19154cdd788c41c0c8cf1
Rafael de Oliveira <rafaeldeoliveira523@gmail.com>
2024-07-13 11:55:18 -0300

    Inicio da Aula
    Iniciando a aula

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,19 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Drawer } from 'expo-router/drawer';
+import { Ionicons } from '@expo/vector-icons';
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+    <Drawer screenOptions={{ headerShown: true }}>
+      <Drawer.Screen
+        name="index" 
+        options={{
+          drawerLabel: 'Início',
+          title: 'Bem vindo!',
+          drawerIcon: () => <Ionicons name="home-outline" size={18} color="#3A98FF" />
+        }}
+      />
+    </Drawer>
+  </GestureHandlerRootView>
   );
 }
